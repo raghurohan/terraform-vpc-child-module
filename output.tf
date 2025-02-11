2ec0c1384a0bbfe67e2a6242b3b78b8bb765e620
raghuatharva <raghuatharva95@gmail.com>
2025-02-11 21:57:09 +0530

    jgf

--- output.tf
+++ output.tf
@@ -14,7 +14,7 @@ output "private_subnet_ids" {
     value = aws_subnet.private[*].id
 }
 
-output "database_subnet_ids" {
+output "mysql_subnet_ids" {
     value = aws_subnet.database[*].id
 }
 output "database_subnet_group" {
